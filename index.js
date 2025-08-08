0f86925d42bcd28f6e5d58b66cd8fcbcb768b9fb
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-08-08 18:14:56 +0600

    multiple parameter used

---
+++ index.js
@@ -0,0 +1,24 @@
+function square(number) {
+    console.log(number);
+
+    const borgo = number * number
+    console.log(borgo);
+
+}
+
+square(5);
+square(50);
+square(25);
+square(285);
+
+
+function add(num1, num2, num3, num4, num5){
+    console.log(num1, num2, num3, num4, num5);
+
+    const total = num1 + num2 + num3 + num4 + num5;
+    console.log(total);
+        
+}
+
+add(41, 78, 65, 90, 15)
+
