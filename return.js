b5ac266e242f5c5c347ef3c789d26d0612e84c8e
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-08-08 19:23:38 +0600

    use return

---
+++ return.js
@@ -0,0 +1,23 @@
+function tenTimes(nmbr) {
+    const result = nmbr * 10;
+    return result;
+}
+
+const outPut = tenTimes(45);
+console.log(outPut);
+
+const outPut2 = tenTimes(99);
+console.log(outPut2);
+
+
+function doMath(num1, num2) {
+    const sum = num1 + num2;
+    const sub = num1 - num2;
+    const mult = num1 * num2;
+    const vag = mult / 2;
+
+    return vag;
+}
+
+const result = doMath(25, 15);
+console.log(result);
