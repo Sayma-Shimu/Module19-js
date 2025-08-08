c3f6e0205101313a582e569204f19b7f28e63a03
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-08-08 19:38:58 +0600

    conditional_return

---
+++ conditional_return.js
@@ -0,0 +1,24 @@
+function isEven(number) {
+    if (number % 2 === 0) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(isEven(5));
+console.log(isEven(8));
+
+
+function isOdd(number) {
+    if (number % 2 === 1) {
+        return true;
+    }
+
+        return false;
+    
+}
+
+console.log(isOdd(50));
+console.log(isOdd(81));
